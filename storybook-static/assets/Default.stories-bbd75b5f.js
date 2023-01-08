3809e20b2ae1a433dfff010cd7e48d257ebdb52a
raihanmuhammad <michael.raihan@stockbit.com>
2023-01-08 18:02:47 +0700

    feat: add script for build production in netlify

---
+++ storybook-static/assets/Default.stories-bbd75b5f.js
@@ -0,0 +1,14 @@
+var a=Object.defineProperty;var t=(e,n)=>a(e,"name",{value:n,configurable:!0});import{j as o}from"./jsx-runtime-920531fb.js";import"./index-d282687f.js";import"./es.object.get-own-property-descriptor-fbf95828.js";function s(){return o("h1",{children:"Halaman Brand"})}t(s,"Brand");const l={parameters:{storySource:{source:`import React from 'react';
+import Badge from './../../../components/Bagde';
+import { ComponentMeta } from '@storybook/react'
+
+export default {
+	title: "Components/Badge/Example",
+	component: Badge
+} as ComponentMeta<typeof Badge>;
+
+export const Success = (args: any) => <Badge {...args}> Success</Badge>
+Success.args = {
+	state: "success",
+}`,locationsMap:{success:{startLoc:{col:23,line:10},endLoc:{col:71,line:10},startBody:{col:23,line:10},endBody:{col:71,line:10}}}}},title:"Components/Badge/Example",component:s},c=t(e=>o(s,{...e,children:" Success"}),"Success");c.args={state:"success"};const i=["Success"];export{c as Success,i as __namedExportsOrder,l as default};
+//# sourceMappingURL=Default.stories-bbd75b5f.js.map
